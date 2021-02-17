d4725234e691d2996bb203af23564851b20243a5
Orlando Marin <orlandojermainemarin@gmail.com>
2021-02-17 11:22:44 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+/*for (let age = 30; age , 40; age++) {
+    console.log("I'm ${age] years old. Happy birthday to me!")
+}*/
+
+
+
+
+function writeCards(stringName, eventName) {
+  const fullPhrase = [];
+  for (let i = 0; i < stringName.length; i++) {
+    fullPhrase.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return fullPhrase;
+}
+
+
+function countDown(){
+    let countDown = 10
+    while (countDown >= 0) {
+        console.log (countDown--)
+    }
+}
